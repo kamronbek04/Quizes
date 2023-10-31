a4484659a83b8bf68b298993d4a4a2f3f45dc9a2
Mekhrojbek <islomovmekhrojbek@gmail.com>
2023-10-31 15:30:13 +0500

    first commit

---
+++ src/components/Result.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="result">
+    <p>Your Result is ...</p>
+    <h1>{{ numberOfCorrectAnswers }}/{{ quizQuestionLength }}</h1>
+    <RouterLink class="back" to="/">Go back</RouterLink>
+  </div>
+</template>
+<script setup>
+import { defineProps } from "vue";
+import { RouterLink } from "vue-router";
+
+const { quizQuestionLength, numberOfCorrectAnswers } = defineProps([
+  "numberOfCorrectAnswers",
+  "quizQuestionLength",
+]);
+</script>
+<style scoped>
+.back {
+  padding: 10px 20px;
+  background-color: red;
+  color: white;
+  border-radius: 10px;
+}
+.result {
+  text-align: center;
+  padding: 100px 0;
+}
+p {
+  font-size: 25px;
+}
+h1 {
+  font-size: 80px;
+}
+</style>
